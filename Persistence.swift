05d2572194c5f42c536468e5d981f44cc35b867d
Rajahiresh <kalvarajahiresh@gmail.com>
2025-08-13 18:03:32 -0500

    New Changes in Project

---
+++ Persistence.swift
@@ -0,0 +1,8 @@
+//
+//  Persistence.swift
+//  Receipt Finder
+//
+//  Created by Rajahiresh Kalva on 8/13/25.
+//
+
+import Foundation
---
+++ ReceiptFinderAppProperties.swift
@@ -0,0 +1,8 @@
+//
+//  ReceiptFinderAppProperties.swift
+//  Receipt Finder
+//
+//  Created by Rajahiresh Kalva on 8/13/25.
+//
+
+import Foundation
